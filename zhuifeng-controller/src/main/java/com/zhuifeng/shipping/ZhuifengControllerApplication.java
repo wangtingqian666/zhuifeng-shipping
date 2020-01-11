c62460d207051fdf084a10f7bee8eff401ca7451
wangtingqian666 <1652679961@qq.com>
2020-01-11 10:06:20 +0800

    汪廷骞 2020年1月11日10点05分 添加一个模块

---
+++ zhuifeng-controller/src/main/java/com/zhuifeng/shipping/ZhuifengControllerApplication.java
@@ -0,0 +1,13 @@
+package com.zhuifeng.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuifengControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuifengControllerApplication.class, args);
+    }
+
+}
